524709a6bf0f4a6d5a5622940e358de4ae30dc2d
tracksafe1 <shikha8883@gmail.com>
2023-11-09 21:07:32 +0000

    homework121

---
+++ homework009/src/midweekhomework/Q23/Bike2.java
@@ -0,0 +1,9 @@
+package midweekhomework.Q23;
+
+class Bike2 extends Vehicle{
+    public void run(){System.out.println("Bike is running safely");}
+    public static void main(String args[]){
+        Bike2 obj = new Bike2();//creating object
+        obj.run();//calling method
+    }
+}
