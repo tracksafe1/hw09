985f6b05a32f47dbd049a8231aaa3f19caecf2d2
tracksafe1 <shikha8883@gmail.com>
2023-11-16 19:30:59 +0000

    homework121

--- homework009/src/inheritance/CodebusterGroups.java
+++ homework009/src/inheritance/CodebusterGroups.java
@@ -6,7 +6,9 @@ public class CodebusterGroups extends Codebuster  {
         t.groupname("Code");
         System.out.println(t.groupstudy("Ayan,Miten,Zarna,Sonal"));
         t.groupname("Restassured");
+        System.out.println(t.groupstudy("kinarri,kulvinder,sumit"));
        t.groupname("code2");
+        System.out.println(t.groupstudy("Hiten,Divya,jwalant"));
     }
 
     @Override
--- homework009/src/polymorphism/Animal.java
+++ homework009/src/polymorphism/Animal.java
@@ -1,5 +1,7 @@
-package polymorphism;
-
+package polymorphism;/*. Write a Java program to create a base class Animal (Animal Family) with a method  called Sound().
+ Create two subclasses Bird and Cat.
+  Override the Sound() method in  each subclass to make a specific sound for each animal.
+*/
 public class Animal {
     public static void main(String[] args) {
         Animal obj=new Animal();
